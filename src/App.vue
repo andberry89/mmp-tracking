9c884bcc6c3d9864214c70b3d9b85f3cca424037
Andrew Berry <andrew.berry@hearst.com>
2025-07-11 15:24:47 -0400

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,177 @@
+<template>
+  <div class="layout-grid">
+    <div id="logo" class="logo"></div>
+    <header>
+      <nav class="wrapper">
+        <ul>
+          <li>MMPs</li>
+          <li>Makes</li>
+          <li>Models</li>
+          <li>Segments</li>
+          <li>Authors</li>
+        </ul>
+      </nav>
+    </header>
+    <aside id="sidebar">
+      <h3>Authors</h3>
+      <div class="sidebar-wrapper">
+        <div class="team">
+          <h4>Buyer's Guide</h4>
+          <div class="author">
+            <span class="author-name">Joey Capparella</span>
+          </div>
+          <div class="author">
+            <span class="author-name">Drew Dorian</span>
+          </div>
+          <div class="author">
+            <span class="author-name">Austin Parsons</span>
+            <span class="assigned">1 Assigned</span>
+          </div>
+          <div class="author">
+            <span class="author-name">Andrew Wendler</span>
+            <span class="assigned">1 Assigned</span>
+          </div>
+        </div>
+        <div class="team">
+          <h4>C/D Staff</h4>
+          <!-- START HERE-->
+        </div>
+      </div>
+    </aside>
+    <main id="content">
+      <p>Main content goes here</p>
+    </main>
+  </div>
+</template>
+<script>
+export default {
+  name: "App",
+};
+</script>
+
+<style lang="scss" scoped>
+.layout-grid {
+  display: grid;
+  grid-template-columns: 140px 1fr;
+  grid-template-rows: auto 1fr;
+  grid-template-areas:
+    "logo header"
+    "sidebar content";
+  height: 100vh;
+
+  #logo {
+    grid-area: logo;
+    background-color: var(--color-background-logo);
+    border-top-left-radius: 8px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-weight: bold;
+    font-size: 2rem;
+  }
+
+  header {
+    grid-area: header;
+    border-top-right-radius: 8px;
+    display: flex;
+    align-items: center;
+    background-color: var(--color-background-header);
+  }
+
+  nav.wrapper {
+    width: 100%;
+    border-bottom: var(--color-border) solid 2px;
+    padding: 3.5rem 20px;
+
+    ul {
+      display: flex;
+      flex-flow: row nowrap;
+      list-style-type: none;
+      margin: 0;
+      padding: 0;
+
+      li {
+        font: 700 2rem/1.2 "Monda", sans-serif;
+        color: var(--color-header-text);
+        margin-right: 2.5rem;
+        cursor: pointer;
+
+        &:hover {
+          color: var(--color-header-text-hover);
+        }
+      }
+    }
+  }
+
+  #sidebar {
+    grid-area: sidebar;
+    background-color: var(--color-background-sidebar);
+    color: var(--color-sidebar-text);
+    padding: 20px 10px;
+
+    h3 {
+      font-size: 12pt;
+      font-weight: 700;
+      color: var(--color-sidebar-text-header);
+      text-transform: uppercase;
+      margin-bottom: 15px;
+    }
+
+    .sidebar-wrapper {
+      padding-left: 5px;
+      font-family: "Asap", sans-serif;
+
+      h4 {
+        font-size: 11pt;
+        font-weight: 700;
+        text-transform: uppercase;
+        color: var(--color-sidebar-text);
+      }
+
+      .author {
+        font-size: 10pt;
+        line-height: 1;
+        color: var(--color-sidebar-text);
+        padding: 8px 0;
+        display: flex;
+        flex-flow: column nowrap;
+        border-bottom: var(--color-sidebar-divider) solid 1px;
+
+        &:last-child {
+          border-bottom: none;
+        }
+
+        .assigned {
+          color: var(--color-sidebar-text-subtext);
+        }
+      }
+    }
+  }
+
+  #content {
+    grid-area: content;
+    color: var(--color-text);
+    padding: 1rem;
+    background: url("@/assets/bg-pattern.jpeg") repeat;
+    background-size: auto;
+  }
+}
+
+// @media (min-width: 1024px) {
+//   header {
+//     display: flex;
+//     place-items: center;
+//     padding-right: calc(var(--section-gap) / 2);
+//   }
+
+//   .logo {
+//     margin: 0 2rem 0 0;
+//   }
+
+//   header .wrapper {
+//     display: flex;
+//     place-items: flex-start;
+//     flex-wrap: wrap;
+//   }
+// }
+</style>
